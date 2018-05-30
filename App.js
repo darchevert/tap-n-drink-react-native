43b0fc3b6f4df12650b932c815a39c37e24b60a3
yoannherlaut <yoann.herlaut@hotmail.fr>
2018-05-30 11:24:39 +0200

    push de mise a jour

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { Font } from 'expo';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import {Font} from 'expo';
 
 export default class App extends React.Component {
 
-
-  constructor(){
-		super();
-		this.state = {
-      casetmp:9,
-			case: []
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      casetmp: 9,
+      case: []
+    };
+  }
 
   // componentDidMount() {
   //     Font.loadAsync({
@@ -22,100 +21,97 @@ export default class App extends React.Component {
   //   }
 
   render() {
-var case = [];
 
-for(var i=0; i<this.state.casetmp; i++ ){
-case.push(<View><Text>tototot</Text></View>)
+    var carre = [];
 
+    for (var i = 0; i < this.state.casetmp; i++) {
+      carre.push(
+        <Text>tototot</Text>
+      );
 
-}
+    }
 
+    return (<View style={styles.container}>
+      <Text h3="h3" style={styles.h3}>Règles du jeu</Text>
+      <View>
 
+        <Text h1="h1" style={styles.h1}>TAP'N'DRINK</Text>
+        <Text text="text" style={styles.text}>AYEZ LA BONNE INTUITION</Text>
 
-    return (
-      <View style={styles.container}>
-<Text h3 style={styles.h3}>Règles du jeu</Text>
-        <View>
+      </View>
 
-          <Text h1 style={styles.h1}>TAP'N'DRINK</Text>
-          <Text text style={styles.text}>AYEZ LA BONNE INTUITION</Text>
+      {carre}
 
-          </View>
+      <Button title="BUTTON_1" loadingProps={{
+          size: "large",
+          color: "rgba(111, 202, 186, 1)"
+        }} titleStyle={{
+          fontWeight: "700"
+        }} buttonStyle={{
+          backgroundColor: "rgba(92, 99,216, 1)",
+          width: 45,
+          height: 45,
+          borderColor: "transparent",
+          borderWidth: 0
+        }} containerStyle={{
+          marginTop: 20
+        }}
 
-          {case}
-          <Button
-            title="BUTTON_1"
-            loadingProps={{ size: "large", color: "rgba(111, 202, 186, 1)" }}
-            titleStyle={{ fontWeight: "700" }}
-            buttonStyle={{
-              backgroundColor: "rgba(92, 99,216, 1)",
-              width: 45,
-              height: 45,
-              borderColor: "transparent",
-              borderWidth: 0,
-            }}
-            containerStyle={{ marginTop: 20 }}
-          />
+      title="REJOUER" color="#475577" />
 
-            title="REJOUER"
-            color="#475577"
-
-
-          />
-
-        </View>
+    </View>
 
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#475577',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  h1: {
-    marginTop: 50,
-    marginBottom: 20,
-    textAlign: 'center',
-    fontSize: 25,
-    color: '#efefef',
-    // fontFamily: 'KaushanScript-Regular',
-  },
-  h2: {
-    textAlign: 'center',
-    fontSize: 30,
-    margin: 80,
-    color: '#efefef',
-    // fontFamily: 'KaushanScript-Regular',
-  },
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#475577',
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
+      h1: {
+        marginTop: 50,
+        marginBottom: 20,
+        textAlign: 'center',
+        fontSize: 25,
+        color: '#efefef',
+        // fontFamily: 'KaushanScript-Regular',
+      },
+      h2: {
+        textAlign: 'center',
+        fontSize: 30,
+        margin: 80,
+        color: '#efefef',
+        // fontFamily: 'KaushanScript-Regular',
+      },
 
-  h3: {
-    textAlign: 'right',
-    fontSize: 25,
-    marginBottom: 50,
-    color: '#efefef',
-    // fontFamily: 'KaushanScript-Regular',
-  },
+      h3: {
+        textAlign: 'right',
+        fontSize: 25,
+        marginBottom: 50,
+        color: '#efefef',
+        // fontFamily: 'KaushanScript-Regular',
+      },
 
-  h4: {
-    textAlign: 'justify',
-    fontSize: 25,
-    marginTop: 200,
-    color: '#efefef',
-    // fontFamily: 'KaushanScript-Regular',
-    lineHeight:100,
-  },
+      h4: {
+        textAlign: 'justify',
+        fontSize: 25,
+        marginTop: 200,
+        color: '#efefef',
+        // fontFamily: 'KaushanScript-Regular',
+        lineHeight: 100
+      },
 
-  text: {
-    textAlign: 'center',
-    fontSize: 10,
-    marginBottom: 70,
-    marginTop: 0,
-    color: '#efefef',
-    // fontFamily: 'OpenSans-Regular',
+      text: {
+        textAlign: 'center',
+        fontSize: 10,
+        marginBottom: 70,
+        marginTop: 0,
+        color: '#efefef',
+        // fontFamily: 'OpenSans-Regular',
 
-  }
-});
+      }
+    });
